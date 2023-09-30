25c5f1da2fe161676ff50508edef2736feb7bac7
David Adiel <davidadiel96@gmail.com>
2023-10-01 00:25:41 +0700

    update: head title dan testing vertical align but failed

--- app/infiniteimagecarousel.jsx
+++ app/infiniteimagecarousel.jsx
@@ -41,18 +41,44 @@ export default function InfiniteImageCarousel() {
     infinite: true,
     slidesToShow: 7,
     slidesToScroll: 1,
-    autoplay: true,
+    // autoplay: true,
     lazyload: "ondemand",
     speed: 2000,
     autoplaySpeed: 2000,
-    cssEase: "linear"
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
 
   return (
     <div className="bg-black">
-      <Slider {...settings}>
+      <Slider {...settings} className='imageSlider'>
         {imageClients.map((imageClient, index, key) => (
-          <div key={index}>
+          <section className='' key={index}>
             <Image
               src={imageClient.url}
               width="100"
@@ -62,7 +88,7 @@ export default function InfiniteImageCarousel() {
               alt={imageClient.alts}
             />
 
-          </div>
+          </section>
         ))}
       </Slider>
     </div>
