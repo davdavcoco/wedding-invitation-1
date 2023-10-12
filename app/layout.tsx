2a9ffc037d013427d8bad0209aa5e8fdfb09cd3a
David Adiel <davidadiel96@gmail.com>
2023-10-12 11:33:07 +0700

    update: about page

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Nav from "./Nav";
 import Footer from "./footer";
 
 export const metadata: Metadata = {
-  title: "Fanrong Studio",
+  title: "Collaborative Practice - Fanrong Studio",
   description: `Explore Fanrong's Studio | Discover a world of innovative design solutions, artistic craftsmanship, and cutting-edge creativity. Our design product studio showcases a diverse range of projects that inspire and captivate. Explore our portfolio today.`,
   keywords: `design, product, studio, portfolio, creativity, innovation`,
 };
